29abfb3f139c00b08229776a1b707f7e401385cf
Md. Shariful Islam <abinsu7@gmail.com>
2024-10-15 12:34:57 +0600

    added Project dto & project service interface

---
+++ ProjectManagement/aspnet-core/src/ProjectManagement.Application.Contracts/Projects/ProjectDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+
+namespace ProjectManagement.Projects
+{
+    public class ProjectDto : AuditedEntityDto<Guid>
+    {
+        [Required]
+        [StringLength(128)]
+        public string Name { get; set; }
+        
+        [Required]
+        [StringLength(10)]
+        public string Code { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+
+        [StringLength(500)]
+        public string Description { get; set; }
+        
+        [Required]
+        public ProjectStatus Status { get; set; }
+    }
+}
