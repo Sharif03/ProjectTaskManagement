1ecaea7e634fdf73e40721414d15e5ebff1ea8c0
Md. Shariful Islam <abinsu7@gmail.com>
2024-10-15 19:40:35 +0600

    created ProjectTask application layer

---
+++ ProjectManagement/aspnet-core/src/ProjectManagement.Application.Contracts/ProjectTasks/GetProjectTaskListDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+
+namespace ProjectManagement.ProjectTasks
+{
+    public class GetProjectTaskListDto : PagedAndSortedResultRequestDto
+    {
+        public List<Guid> ProjectIds { get; set; } = new List<Guid>(); // Multi-project filter
+        public string? Filter { get; set; } // search name & code
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+    }
+}
